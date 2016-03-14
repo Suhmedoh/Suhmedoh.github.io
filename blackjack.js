8ee4e94ce4a8861f2aa5d4a10f4ec2589a837ea0
Suhmedoh <joshsemedo@gmail.com>
2016-03-14 18:07:33 -0400

    using objects for player and dealer

--- blackjack.js
+++ blackjack.js
@@ -5,18 +5,19 @@ function displayRules() {
 // card suits: spades = &#9824; , hearts &#9825; , clubs &#9827 , diamonds &#9826
 var cards = ['A&#9824;' , '2&#9824;' , '3&#9824;' , '4&#9824;' , '5&#9824;' , '6&#9824;' , '7&#9824;' , '8&#9824;' , '9&#9824;' , '10&#9824;' , 'J&#9824;' , 'Q&#9824;' , 'K&#9824;' , 'A&#9825;' , '2&#9825;' , '3&#9825;' , '4&#9825;' , '5&#9825;' , '6&#9825;' , '7&#9825;' , '8&#9825;' , '9&#9825;' , '10&#9825;', 'J&#9825;' , 'Q&#9825;' , 'K&#9825;' , 'A&#9827;' , '2&#9827;' , '3&#9827;' , '4&#9827;' , '5&#9827;' , '6&#9827;' , '7&#9827;' , '8&#9827;' , '9&#9827;' , '10&#9827;' , 'J&#9827;' , 'Q&#9827;' , 'K&#9827;' , 'A&#9826;' , '2&#9826;' , '3&#9826;' , '4&#9826;' , '5&#9826;' , '6&#9826;' , '7&#9826;' , '8&#9826;' , '9&#9826;' , '10&#9826;' , 'J&#9826;' , 'Q&#9826;' , 'K&#9826;' ];
 
-dCard1 = cards[Math.floor(Math.random() * cards.length)];
-dCard2 = cards[Math.floor(Math.random() * cards.length)];
-pCard1 = cards[Math.floor(Math.random() * cards.length)];
-pCard2 = cards[Math.floor(Math.random() * cards.length)];
+
+var dealer = new Object();
+dealer.card1 = cards[Math.floor(Math.random() * cards.length)];;
+dealer.card2 = cards[Math.floor(Math.random() * cards.length)];;
+var player = new Object();
+player.card1 = cards[Math.floor(Math.random() * cards.length)];
+player.card2 = cards[Math.floor(Math.random() * cards.length)];
+document.getElementById("dealerCard1").innerHTML = dealer.card1;
+document.getElementById("dealerCard2").innerHTML = dealer.card2;
+document.getElementById("playerCard1").innerHTML = player.card1;
+document.getElementById("playerCard2").innerHTML = player.card2;
 
 
-document.getElementById("dealerCard1").innerHTML = dCard1;
-document.getElementById("dealerCard2").innerHTML = dCard2;
-document.getElementById("playerCard1").innerHTML = pCard1;
-document.getElementById("playerCard2").innerHTML = pCard2;
 
-var dealerCard = new Object();
-	dealerCard.pip = dCard1;
-	dealerCard.val = dCard1.cardAt(0);
-	
+
+

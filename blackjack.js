d50c843181b1eda0cd608aebffd1a07fdf966918
Suhmedoh <joshsemedo@gmail.com>
2016-03-14 17:26:54 -0400

    separated files, basic UI done

---
+++ blackjack.js
@@ -0,0 +1,17 @@
+
+function displayRules() {
+	alert('Welcome to Joshs rendition of Blackjack.\n\n The rules are as follows:\n You must try to get as close to 21 as you can, without going over.\n\n Each card has a numerical value; 2 through 9 represent themselves, 10 through King are valued at 10, and an Ace is counted at 1 or 11, depending on which is more beneficial.\n\n You are dealt 2 cards; For the purpose of this game, both cards will be faceup, as it doesnt matter whether or not the dealer can see your cards, because they have to follow a predefined set of rules. \n\nAfter you recieve your two cards and the dealer has theirs, you may hit or stay.\n\n  If you choose hit, you are dealt another cardwhich adds to your total.\n\n  If at any point you go over 21, you lose. \n If you stay, you stick with the cards you have and whatever they total up to.\n\n  The goal is to beat the dealer.  This can be done by getting 21 exactly blackjack, getting a higher card value total than the dealer .. for example, your 20 vs their 19.. or by not going over 21 and having the dealer bust.')}
+// make the array of cards.  Seeing as a casinos typically use 6 or more decks and shuffle the cards partway through, i'll use a theoretically "endless" deck, with an unlimited number of shuffled cards.
+// card suits: spades = &#9824; , hearts &#9825; , clubs &#9827 , diamonds &#9826
+var cards = ['A&#9824;' , '2&#9824;' , '3&#9824;' , '4&#9824;' , '5&#9824;' , '6&#9824;' , '7&#9824;' , '8&#9824;' , '9&#9824;' , '10&#9824;' , 'J&#9824;' , 'Q&#9824;' , 'K&#9824;' , 'A&#9825;' , '2&#9825;' , '3&#9825;' , '4&#9825;' , '5&#9825;' , '6&#9825;' , '7&#9825;' , '8&#9825;' , '9&#9825;' , '10&#9825;', 'J&#9825;' , 'Q&#9825;' , 'K&#9825;' , 'A&#9827;' , '2&#9827;' , '3&#9827;' , '4&#9827;' , '5&#9827;' , '6&#9827;' , '7&#9827;' , '8&#9827;' , '9&#9827;' , '10&#9827;' , 'J&#9827;' , 'Q&#9827;' , 'K&#9827;' , 'A&#9826;' , '2&#9826;' , '3&#9826;' , '4&#9826;' , '5&#9826;' , '6&#9826;' , '7&#9826;' , '8&#9826;' , '9&#9826;' , '10&#9826;' , 'J&#9826;' , 'Q&#9826;' , 'K&#9826;' ];
+
+dCard1 = cards[Math.floor(Math.random() * cards.length)];
+dCard2 = cards[Math.floor(Math.random() * cards.length)];
+pCard1 = cards[Math.floor(Math.random() * cards.length)];
+pCard2 = cards[Math.floor(Math.random() * cards.length)];
+
+
+document.getElementById("dealerCard1").innerHTML = dCard1;
+document.getElementById("dealerCard2").innerHTML = dCard2;
+document.getElementById("playerCard1").innerHTML = pCard1;
+document.getElementById("playerCard2").innerHTML = pCard2;

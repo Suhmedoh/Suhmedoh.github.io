a8f56f9abb3d097606e78a579ed8cef289ba1121
Josh Semedo <joshsemedo@gmail.com>
2016-11-01 14:14:05 -0400

    testing the table function

--- gui_class/js/mult.js
+++ gui_class/js/mult.js
@@ -19,14 +19,14 @@ function updateTable() {
 
   multTable = "<table><tr>";
 
-  for (i = 0, j = 0; i <= arrayHoriz.len(); i++, j++) {
+  for (i = 0, j = 0; i <= arrayHoriz.length; i++, j++) {
   	multTable += "<th> $arrayHoriz[j] </th>";
   }
 
   multTable += "</tr><tr>";
 
-  for (i = 0, k = 0; i <= arrayVerti.len(); i++, k++) {
-    for (i = 0, j = 0; i <= arrayHoriz.len(); i++, j++) {
+  for (i = 0, k = 0; i <= arrayVerti.length; i++, k++) {
+    for (i = 0, j = 0; i <= arrayHoriz.length; i++, j++) {
       multTable += "<td> $arrayHoriz[j]*$arrayHoriz[k] </td>";
     }
   }

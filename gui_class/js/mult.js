1425eeeab9602682228a16804fd1d524080c1969
Josh Semedo <joshsemedo@gmail.com>
2016-11-01 14:54:28 -0400

    testing the table function

--- gui_class/js/mult.js
+++ gui_class/js/mult.js
@@ -10,11 +10,11 @@ function updateTable() {
   var multTable;
   var table = document.getElementById('multTable');
 
-  for (i = horizValue1, j = 0; i <= horizValue2; i++, j++) {
+  for (i = horizValue1, j = 0; i < horizValue2; i++, j++) {
   	arrayHoriz[j] = i;
   }
 
-  for (i = vertiValue1, k = 0; i <= vertiValue2; i++, k++) {
+  for (i = vertiValue1, k = 0; i < vertiValue2; i++, k++) {
   	arrayVerti[k] = i;
   }
 
@@ -28,8 +28,8 @@ function updateTable() {
 
   for (k = 0; k <= arrayVerti.length; k++){
     multTable += "<td>" + arrayVerti[k] + "</td>";
-      for (i = 0; i <= arrayVerti.length; i++) {
-        for (j = 0; j <= arrayHoriz.length; j++) {
+      for (i = 0; i < arrayVerti.length; i++) {
+        for (j = 0; j < arrayHoriz.length; j++) {
           console.log("ah*av=" + arrayHoriz[j]*arrayVerti[i])
           multTable += "<td>" + arrayHoriz[j]*arrayVerti[i] + "</td>";
         }

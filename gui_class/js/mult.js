de1a4bcf3d13d28df9144d6b5e767b0ed9a42278
Josh Semedo <joshsemedo@gmail.com>
2016-11-02 16:15:37 -0400

    added lots of new styling, fixed error checking a bit more, added some jquery animation effects

--- gui_class/js/mult.js
+++ gui_class/js/mult.js
@@ -5,16 +5,20 @@
 //create a function which creates and updates the table
 function updateTable() {
   //declare all necessar variables
-  var horizValue1 = document.getElementById("horiz1").value;
-  var horizValue2 = document.getElementById("horiz2").value;
-  var vertiValue1 = document.getElementById("verti1").value;
-  var vertiValue2 = document.getElementById("verti2").value;
+  var hv1 = document.getElementById("horiz1").value;
+  var hv2 = document.getElementById("horiz2").value;
+  var vv1 = document.getElementById("verti1").value;
+  var vv2 = document.getElementById("verti2").value;
   var arrayHoriz = new Array();
   var arrayVerti = new Array();
   var multTable;
   var error;
   var table = document.getElementById('multTable');
-
+  //turn input into ints
+  var horizValue1 = parseInt(hv1);
+  var horizValue2 = parseInt(hv2);
+  var vertiValue1 = parseInt(vv1);
+  var vertiValue2 = parseInt(vv2);
   //handling errors
   if ((horizValue1 == horizValue2) || (vertiValue1 == vertiValue2)) {
     error = "The starting and ending range values can not be the same";
@@ -72,3 +76,191 @@ function updateTable() {
 }
 
 
+//some style things
+
+$(document).ready(function(){
+  $("#horiz1").mouseenter(function(){
+    $('#horiz1').css({
+      'font-family': 'Dosis',
+      'display': 'inline-block',
+      'padding': 0,
+      'width': 100,
+      'height': 50,
+      'background-color': 'transparent',
+      'border-color': '#e28e7f',
+      'border-radius': 1,
+      'border-right-style': 'solid',
+      'border-right-width': 3,
+      'border-left-style': 'solid',
+      'border-left-width': 3,
+      'border-top-style': 'solid',
+      'border-top-width': 1,
+      'border-bottom-style': 'solid',
+      'border-bottom-width': 1,
+      'text-align': 'center',
+      'font-size': 24,
+      'transition-duration': '.3s' 
+    });
+  }); 
+  $('#horiz1').mouseleave(function(){
+    $('#horiz1').css({
+      'font-family': 'Dosis',
+      'display': 'inline-block',
+      'padding': 0,
+      'width': 100,
+      'height': 50,
+      'background-color': 'transparent',
+      'border-color': '#778a53',
+      'border-radius': 1,
+      'border-right-style': 'solid',
+      'border-right-width': 7,
+      'border-left-style': 'solid',
+      'border-left-width': 7,
+      'border-top-style': 'solid',
+      'border-top-width': 2,
+      'border-bottom-style': 'solid',
+      'border-bottom-width': 2,
+      'text-align': 'center',
+      'font-size': 24,
+      'transition-duration': '.3s'
+    });
+  });
+  $("#horiz2").mouseenter(function(){
+    $('#horiz2').css({
+      'font-family': 'Dosis',
+      'display': 'inline-block',
+      'padding': 0,
+      'width': 100,
+      'height': 50,
+      'background-color': 'transparent',
+      'border-color': '#e28e7f',
+      'border-radius': 1,
+      'border-right-style': 'solid',
+      'border-right-width': 3,
+      'border-left-style': 'solid',
+      'border-left-width': 3,
+      'border-top-style': 'solid',
+      'border-top-width': 1,
+      'border-bottom-style': 'solid',
+      'border-bottom-width': 1,
+      'text-align': 'center',
+      'font-size': 24,
+      'transition-duration': '.3s' 
+    });
+  }); 
+  $('#horiz2').mouseleave(function(){
+    $('#horiz2').css({
+      'font-family': 'Dosis',
+      'display': 'inline-block',
+      'padding': 0,
+      'width': 100,
+      'height': 50,
+      'background-color': 'transparent',
+      'border-color': '#778a53',
+      'border-radius': 1,
+      'border-right-style': 'solid',
+      'border-right-width': 7,
+      'border-left-style': 'solid',
+      'border-left-width': 7,
+      'border-top-style': 'solid',
+      'border-top-width': 2,
+      'border-bottom-style': 'solid',
+      'border-bottom-width': 2,
+      'text-align': 'center',
+      'font-size': 24,
+      'transition-duration': '.3s'
+    });
+  });
+  $("#verti1").mouseenter(function(){
+    $('#verti1').css({
+      'font-family': 'Dosis',
+      'display': 'inline-block',
+      'padding': 0,
+      'width': 100,
+      'height': 50,
+      'background-color': 'transparent',
+      'border-color': '#e28e7f',
+      'border-radius': 1,
+      'border-right-style': 'solid',
+      'border-right-width': 3,
+      'border-left-style': 'solid',
+      'border-left-width': 3,
+      'border-top-style': 'solid',
+      'border-top-width': 1,
+      'border-bottom-style': 'solid',
+      'border-bottom-width': 1,
+      'text-align': 'center',
+      'font-size': 24,
+      'transition-duration': '.3s' 
+    });
+  }); 
+  $('#verti1').mouseleave(function(){
+    $('#verti1').css({
+      'font-family': 'Dosis',
+      'display': 'inline-block',
+      'padding': 0,
+      'width': 100,
+      'height': 50,
+      'background-color': 'transparent',
+      'border-color': '#778a53',
+      'border-radius': 1,
+      'border-right-style': 'solid',
+      'border-right-width': 7,
+      'border-left-style': 'solid',
+      'border-left-width': 7,
+      'border-top-style': 'solid',
+      'border-top-width': 2,
+      'border-bottom-style': 'solid',
+      'border-bottom-width': 2,
+      'text-align': 'center',
+      'font-size': 24,
+      'transition-duration': '.3s'
+    });
+  });
+  $("#verti2").mouseenter(function(){
+    $('#verti2').css({
+      'font-family': 'Dosis',
+      'display': 'inline-block',
+      'padding': 0,
+      'width': 100,
+      'height': 50,
+      'background-color': 'transparent',
+      'border-color': '#e28e7f',
+      'border-radius': 1,
+      'border-right-style': 'solid',
+      'border-right-width': 3,
+      'border-left-style': 'solid',
+      'border-left-width': 3,
+      'border-top-style': 'solid',
+      'border-top-width': 1,
+      'border-bottom-style': 'solid',
+      'border-bottom-width': 1,
+      'text-align': 'center',
+      'font-size': 24,
+      'transition-duration': '.3s' 
+    });
+  }); 
+  $('#verti2').mouseleave(function(){
+    $('#verti2').css({
+      'font-family': 'Dosis',
+      'display': 'inline-block',
+      'padding': 0,
+      'width': 100,
+      'height': 50,
+      'background-color': 'transparent',
+      'border-color': '#778a53',
+      'border-radius': 1,
+      'border-right-style': 'solid',
+      'border-right-width': 7,
+      'border-left-style': 'solid',
+      'border-left-width': 7,
+      'border-top-style': 'solid',
+      'border-top-width': 2,
+      'border-bottom-style': 'solid',
+      'border-bottom-width': 2,
+      'text-align': 'center',
+      'font-size': 24,
+      'transition-duration': '.3s'
+    });
+  });
+});

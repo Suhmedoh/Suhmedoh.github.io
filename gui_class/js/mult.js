c5c6853f778e68350673a46ffb839c815aaeb9e4
Josh Semedo <joshsemedo@gmail.com>
2016-11-02 13:26:14 -0400

    finished generating mult table

--- gui_class/js/mult.js
+++ gui_class/js/mult.js
@@ -8,37 +8,51 @@ function updateTable() {
   var arrayHoriz = new Array();
   var arrayVerti = new Array();
   var multTable;
+  var error;
   var table = document.getElementById('multTable');
 
-  for (i = horizValue1, j = 0; i <= horizValue2; i++, j++) {
-  	arrayHoriz[j] = i;
+  if ((horizValue1 == horizValue2) || (vertiValue1 == vertiValue2)) {
+    error = "The starting and ending range values can not be the same";
+    document.getElementById("table").innerHTML = error;
   }
 
-  for (i = vertiValue1, k = 0; i <= vertiValue2; i++, k++) {
-  	arrayVerti[k] = i;
+  else if ((horizValue1 > horizValue2) || (vertiValue1 > vertiValue2)) {
+    error = "Please enter an increasing range, from smaller number to bigger";
+    document.getElementById("table").innerHTML = error;
   }
 
-  multTable = "<table><tr><td>&nbsp;</td>";
+  else {
 
-  for (i = 0; i <= arrayHoriz.length - 1; i++) {
-  	multTable += "<td>" + arrayHoriz[i] + " </td>";
-  }
-
-  multTable += "</tr><tr>";
-
-  for (i = 0, k = 0; i <= arrayVerti.length - 1; i++, k++) {
-    multTable += "<td>" + arrayVerti[i] + "</td>";
-    for (j = 0; j <= arrayHoriz.length - 1; j++) {
-      console.log("ah*av=" + arrayHoriz[j]*arrayVerti[i])
-      multTable += "<td>" + arrayHoriz[j]*arrayVerti[i] + "</td>";
+    for (i = horizValue1, j = 0; i <= horizValue2; i++, j++) {
+    	arrayHoriz[j] = i;
     }
-    multTable += "</tr><tr>"
+
+    for (i = vertiValue1, k = 0; i <= vertiValue2; i++, k++) {
+    	arrayVerti[k] = i;
+    }
+
+    multTable = "<table><tr><td>&nbsp;</td>";
+
+    for (i = 0; i <= arrayHoriz.length - 1; i++) {
+    	multTable += "<td>" + arrayHoriz[i] + " </td>";
+    }
+
+    multTable += "</tr><tr>";
+
+    for (i = 0, k = 0; i <= arrayVerti.length - 1; i++, k++) {
+      multTable += "<td>" + arrayVerti[i] + "</td>";
+      for (j = 0; j <= arrayHoriz.length - 1; j++) {
+        console.log("ah*av=" + arrayHoriz[j]*arrayVerti[i])
+        multTable += "<td>" + arrayHoriz[j]*arrayVerti[i] + "</td>";
+      }
+      multTable += "</tr><tr>"
+    }
+
+
+    multTable += "</tr></table>";
+
+    document.getElementById("table").innerHTML = multTable;
   }
-
-
-  multTable += "</tr></table>";
-
-  document.getElementById("table").innerHTML = multTable;
 
   return false;
 }
